eb375648c6290e3bc2096773d4204a369b899f3a
Mike Salvato <msalvato@mit.edu>
2014-11-05 20:24:37 -0500

    min cube size to fit all the data

--- gpu/kinfu/src/internal.h
+++ gpu/kinfu/src/internal.h
@@ -57,7 +57,7 @@ namespace pcl
     const int DIVISOR = 32767;     // SHRT_MAX;
 
 	//Should be multiple of 32
-    enum { VOLUME_X = 512, VOLUME_Y = 512, VOLUME_Z = 512 };
+    enum { VOLUME_X = 992, VOLUME_Y = 992, VOLUME_Z = 992 };
 
 	
     const float VOLUME_SIZE = 3.0f; // in meters
